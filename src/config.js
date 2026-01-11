27187866c196f94a326e5d894577777ac0058cad
redgodme <129983944+redgodme@users.noreply.github.com>
2026-01-11 12:06:36 +0800

    更新 config.js

--- src/config.js
+++ src/config.js
@@ -73,7 +73,7 @@ const config = {
         musicPlayer: {
                 server: 'netease',  //服务提供商 --网易云音乐
                 type: 'playlist',   //歌单类型
-                id: '2028178887'  //歌单id ---> music.163.com/#/playlist?id=2028178887
+                id: '17657816544'  //歌单id ---> music.163.com/#/playlist?id=2028178887
         },
 
         //壁纸数据 -----可以将壁纸文件上传到图床获取网络直链。若想调用api，请前往脚本自行修改逻辑
