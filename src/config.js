34d7b1dd8e4ac998bce64bf2c44f160f7f91bd73
redgodme <129983944+redgodme@users.noreply.github.com>
2026-01-12 00:38:44 +0800

    更新 config.js

--- src/config.js
+++ src/config.js
@@ -137,9 +137,9 @@ const config = {
         },
         //项目卡片 其中 字段"show"控制初始卡片的text是否展开
         projectcards: [
-                { go: "🚀 前往", img: "go1768148017075_dreamina_7567801047083748654_1768147954510_m.mp4", title: "施工ing", subtitle: "1,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://to.xiaoyin.de5.net", show: false },
+                { go: "🚀 前往", img: "https://url.xiaoyin.de5.net/file/1768142657673_1768059050314.jpg", title: "施工ing", subtitle: "1,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://to.xiaoyin.de5.net", show: false },
                 { go: "🗂️ 前往", img: "https://cloudflare-imgbed-a0l.pages.dev/file/1768138557250_3100x1775.jpg", title: "图床云盘", subtitle: "2,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://url.xiaoyin.de5.net/", show: false },
-                { go: "📝 前往", img: "/img/sunshine.jpg", title: "施工ing", subtitle: "3,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://to.xiaoyin.de5.net", show: false },
+                { go: "📝 前往", img: "https://url.xiaoyin.de5.net/file/1768142652035_1768059062019.jpg", title: "施工ing", subtitle: "3,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://to.xiaoyin.de5.net", show: false },
                 { go: "👍 前往", img: "/img/sunshine.jpg", title: "快完成了", subtitle: "4,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://to.xiaoyin.de5.net", show: false },
                 { go: "🗃 前往", img: "/img/sunshine.jpg", title: "影视备用", subtitle: "5,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://tv.redgod.qzz.io/login", show: false },
                 { go: "🎨 前往", img: "/img/sunshine.jpg", title: "订阅后台", subtitle: "6,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://gxcm.mygad.de5.net/login", show: false },
