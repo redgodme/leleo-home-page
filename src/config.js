3fbf2dcd652b7ced18d37193a7a63ee2d9dfaf49
redgodme <129983944+redgodme@users.noreply.github.com>
2026-01-12 23:11:51 +0800

    更新 config.js

--- src/config.js
+++ src/config.js
@@ -31,7 +31,7 @@ const config = {
                         "datainfo": {
                                 "title": "海洋女孩",
                                 "preview": "/img/wallpaper/static/海洋女孩/image-pre.webp",
-                                "url": "/img/wallpaper/static/海洋女孩/image.png",     //当然，也可填写网络地址或壁纸api，如随机PC壁纸api："url":"https://t.mwm.moe/pc"
+                                "url": "https://url.xiaoyin.de5.net/file/1768230630058_94036.mp4",     //当然，也可填写网络地址或壁纸api，如随机PC壁纸api："url":"https://t.mwm.moe/pc"
                         },
                 },
                 "mobile": {   //移动端
